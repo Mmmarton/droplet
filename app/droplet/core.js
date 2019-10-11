3cd67fb9e8ec786dc25772d592729884a4f1d707
Marton Cserei <marton.cserei@iquestgroup.com>
2019-10-11 16:14:36 +0300

    start from scratch 😄

--- app/droplet/core.js
+++
@@ -1,9 +0,0 @@
-const droplet = (function(global) {
-  var droplet = {
-    version: '0.0.1'
-  };
-  global.droplet = droplet;
-  return droplet;
-})(typeof window === 'undefined' ? global : window);
-
-export default droplet;
--- test/components.test.js
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import droplet from '../app/droplet/core';
-import '../app/droplet/components';
-
-describe('components', () => {
-  it('is initialized', () => {
-    expect(droplet.components).to.not.be.undefined;
-  });
-});
