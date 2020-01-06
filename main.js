f5306da0a3a1f658dd5c84faf086c152ae550740
Marton Cserei <marton.cserei@iquestgroup.com>
2020-01-06 14:10:55 +0200

    switch to class-based structure

--- main.js
+++ main.js
@@ -1,54 +1,65 @@
+'use strict';
+
 const isEvent = key => key.startsWith('on');
 
-function createComponent(structure) {
-  const { type, props, children } = structure;
-  let element;
+function componentToNode(component) {
+  const { type, props, children } = component.template || component;
+  let node;
 
   if (!type) {
-    element = document.createTextNode(structure);
+    node = document.createTextNode(component);
   } else {
-    element = document.createElement(type);
-  }
+    node = document.createElement(type);
+    if (props) {
+      Object.keys(props).forEach(key => {
+        if (isEvent(key)) {
+          const eventType = key.toLowerCase().substring(2);
+          node.addEventListener(eventType, props[key]);
+        } else {
+          node[key] = props[key];
+        }
+      });
+    }
 
-  if (props) {
-    Object.keys(props).forEach(key => {
-      if (isEvent(key)) {
-        const eventType = key.toLowerCase().substring(2);
-        document.addEventListener(eventType, props[key]);
-      } else {
-        element[key] = props[key];
+    if (children) {
+      for (let i = 0; i < children.length; i++) {
+        let child = children[i];
+        node.appendChild(componentToNode(child));
       }
-    });
-  }
-
-  if (children) {
-    for (let i = 0; i < children.length; i++) {
-      let child = children[i];
-      element.appendChild(createComponent(child));
     }
   }
-  return element;
+
+  return node;
 }
 
 // the application
 
 const root = document.querySelectorAll('body')[0];
-const main = {
-  type: 'div',
-  children: [
-    {
-      type: 'h1',
-      props: {
-        className: 'main-title',
-        onClick: () => console.log('heya')
+
+class Component {
+  template = {
+    type: 'div',
+    children: [
+      {
+        type: 'h1',
+        props: {
+          className: 'main-title',
+          onClick: this.handle
+        },
+        children: ['title']
       },
-      children: ['title']
-    },
-    {
-      type: 'p',
-      children: ['some content']
-    }
-  ]
-};
-let mainComponent = createComponent(main);
-root.appendChild(mainComponent);
+      {
+        type: 'p',
+        children: ['some content']
+      }
+    ]
+  };
+
+  handle() {
+    console.log('BOO');
+  }
+}
+
+let mainComponent = new Component();
+let mainNode = componentToNode(mainComponent.template);
+root.appendChild(mainNode);
