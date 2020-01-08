cce1fdec954cec0f6c944ec263f4e9230afdab31
Marton Cserei <marton.cserei@iquestgroup.com>
2020-01-08 15:52:10 +0200

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {}
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
