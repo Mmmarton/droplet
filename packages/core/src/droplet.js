b116408141c1c6bb7e1504272ab704c375035ac6
Marton Cserei <marton.cserei@iquestgroup.com>
2020-04-19 15:59:50 +0300

    feature: write actual frontpage text and tweak style

--- packages/core/src/droplet.js
+++ packages/core/src/droplet.js
@@ -1,142 +1,473 @@
-'use strict';
-/*
- * Experimental in-between build.
- * Manipulating DOM elements directly.
- */
-import { html2json } from './template-parser';
+class NodeSet {
+  list = [];
 
-let renderQueue = [];
-let componentsList = [];
-let firstRender;
-
-function renderIntoBody(component) {
-  let body = document.querySelectorAll('body')[0];
-  while (body.firstChild) {
-    body.removeChild(body.firstChild);
+  get size() {
+    return this.list.length;
   }
-  firstRender = () => {
-    body.appendChild(component.template.node);
-  };
-  addNodeToRenderQueue(component.template, component, { node: body });
+
+  add(value) {
+    for (let i = 0; i < this.list.length; i++) {
+      if (
+        this.list[i].node === value.node &&
+        this.list[i].object === value.object
+      ) {
+        this.list[i] = value;
+        return;
+      }
+    }
+    this.list.push(value);
+  }
+
+  pop() {
+    return this.list.pop();
+  }
 }
 
-let isSimpleExpression = string =>
-  string[0].startsWith('{') && string.endsWith('}');
+let processQueue = new NodeSet();
+let renderQueue = new Set();
+//-----------------------------html parsing--------------------------------
+function createDOMNodeFromHTML(htmlString) {
+  let div = document.createElement('div');
+  div.innerHTML = htmlString;
+  return div.firstElementChild;
+}
 
-function insertFieldsIntoString(string = '', object = {}) {
-  if (isSimpleExpression(string)) {
-    let field = string.substring(1, string.length - 1);
-    if (field.endsWith('()')) {
-      let key = field.substring(0, field.length - 2);
-      if (typeof object.getAttribute(key) === 'function') {
-        return object.getAttribute(key);
+function createNodefromDOMNode(DOMNode, componentsList) {
+  let node;
+  if (DOMNode.attributes.hasOwnProperty('*if')) {
+    node = buildIfNode(DOMNode, componentsList);
+  } else if (DOMNode.attributes.hasOwnProperty('*for')) {
+    node = buildForNode(DOMNode, componentsList);
+  } else {
+    node = buildNormalNode(DOMNode, componentsList);
+  }
+
+  return node;
+}
+
+function buildNormalNode(DOMNode, componentsList) {
+  let node = {
+    elementName: DOMNode.nodeName,
+    attributes: {},
+    children: [],
+    DOMNode,
+    component: componentsList[DOMNode.nodeName],
+  };
+
+  initializeNodeAttributes(node);
+
+  if (node.component) {
+    initializeNodeComponent(node);
+  } else {
+    initializeChildNodes(node, componentsList);
+  }
+
+  return node;
+}
+
+function buildIfNode(DOMNode, componentsList) {
+  let expression = DOMNode.attributes['*if'].value;
+  DOMNode.removeAttribute('*if');
+  let node = {
+    elementName: '*if',
+    expression,
+    active: null,
+    placeholder: document.createTextNode(''),
+    content: createNodefromDOMNode(DOMNode, componentsList),
+  };
+  return node;
+}
+
+function buildForNode(DOMNode, componentsList) {
+  let expression = DOMNode.attributes['*for'].value;
+  DOMNode.removeAttribute('*for');
+  let node = {
+    elementName: '*for',
+    expression,
+    elements: {},
+    placeholder: document.createTextNode(''),
+    content: createNodefromDOMNode(DOMNode, componentsList),
+  };
+
+  DOMNode.replaceWith(node.placeholder);
+  return node;
+}
+
+function initializeNodeAttributes(node) {
+  Object.keys(node.DOMNode.attributes).forEach((key) => {
+    let attribute = node.DOMNode.attributes[key];
+    node.attributes[attribute.name] = attribute.value;
+    if (attribute.name.startsWith('on')) {
+      node.DOMNode[attribute.name] = null;
+    }
+  });
+}
+
+function initializeNodeComponent(node) {
+  node.instance = new node.component();
+  while (node.DOMNode.firstChild) {
+    node.DOMNode.removeChild(node.DOMNode.firstChild);
+  }
+  node.DOMNode.appendChild(node.instance.template.DOMNode);
+  node.children.push(node.instance.template);
+}
+
+function initializeChildNodes(node, componentsList) {
+  Object.keys(node.DOMNode.childNodes).forEach((key) => {
+    let child = buildChildNode(node.DOMNode.childNodes[key], componentsList);
+    if (child) {
+      node.children.push(child);
+    }
+  });
+}
+
+function buildChildNode(DOMNode, componentsList) {
+  let nodeName = DOMNode.nodeName;
+  if (nodeName === '#text') {
+    let text = DOMNode.nodeValue.replace(/(\n|\r)/gm, '').trim();
+    if (text) {
+      return { text, DOMNode };
+    }
+  } else if (!nodeName.startsWith('#')) {
+    return createNodefromDOMNode(DOMNode, componentsList);
+  }
+  return null;
+}
+
+function html2json(html, componentsList) {
+  return createNodefromDOMNode(createDOMNodeFromHTML(html), componentsList);
+}
+
+//BEGIN----------------------update and render logic-------------------------
+function getObjectAttribute(object, attribute) {
+  if (!attribute) {
+    return null;
+  }
+
+  let value = object;
+  let properties = attribute.split('.');
+
+  for (let property of properties) {
+    if (property[property.length - 1] === ')') {
+      value = value[property.substring(0, property.length - 2)];
+      if (value) {
+        value = value();
+      }
+    } else {
+      if (value) {
+        value = value[property];
       }
     }
   }
 
+  return value;
+}
+
+function updateNode({ node, object }) {
+  if (node.elementName === '*if') {
+    updateIfNode(node, object);
+  }
+
+  if (node.elementName === '*for') {
+    updateForNode(node, object);
+  }
+
+  if (node.text) {
+    updateTextNode(node, object);
+    return;
+  }
+
+  if (node.attributes) {
+    object = updateNodeAttributes(node, object);
+  }
+
+  if (node.children) {
+    node.children.forEach((child) => {
+      processQueue.add({
+        node: child,
+        object,
+      });
+    });
+  }
+}
+
+function updateNodeAttributes(node, object) {
+  Object.keys(node.attributes).forEach((attribute) => {
+    let value = node.attributes[attribute];
+    if (value[0] === '{') {
+      value = value.substring(1, value.length - 1);
+      value = getObjectAttribute(object, value);
+
+      if (attribute.startsWith('on')) {
+        if (!node.DOMNode[attribute]) {
+          node.newDOMNode = node.DOMNode.cloneNode(false);
+          node.newDOMNode.removeAttribute(attribute);
+          node.newDOMNode[attribute] = value;
+          renderQueue.add(node);
+        }
+      } else if (getObjectAttribute(node.DOMNode, attribute) !== value) {
+        node.newDOMNode = node.DOMNode.cloneNode(false);
+        node.newDOMNode.setAttribute(attribute, value);
+        renderQueue.add(node);
+      }
+
+      if (node.component) {
+        node.instance.inputs[attribute] = value;
+      }
+    }
+  });
+
+  if (node.component) {
+    object = node.instance;
+  }
+  return object;
+}
+
+function renderNode(node) {
+  if (node.toBeDeleted) {
+    node.DOMNode.remove();
+    node.toBeDeleted = false;
+  } else if (node.insertAfterNode) {
+    node.insertAfterNode.after(node.DOMNode);
+    node.insertAfterNode = null;
+  } else {
+    if (!node.skipChildren && node.children) {
+      node.children.forEach((child) => {
+        node.newDOMNode.appendChild(child.DOMNode);
+      });
+    }
+    node.DOMNode.replaceWith(node.newDOMNode);
+    node.DOMNode = node.newDOMNode;
+  }
+}
+
+function workLoop(deadline) {
+  let thereIsStillTime = true;
+  while (processQueue.size && thereIsStillTime) {
+    let elementToProcess = processQueue.pop();
+    updateNode(elementToProcess);
+    thereIsStillTime = deadline.timeRemaining() > 0;
+  }
+  while (renderQueue.size && thereIsStillTime) {
+    let elementToRender = renderQueue.entries().next().value[0];
+    renderQueue.delete(elementToRender);
+    renderNode(elementToRender);
+
+    thereIsStillTime = deadline.timeRemaining() > 0;
+  }
+
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+//END------------------------update and render logic-------------------------
+
+//BEGIN-------------------------for node update------------------------------
+function updateForNode(node, object) {
+  let [iterator, listName] = node.expression.split(' of ');
+
+  let objectAttribute = getObjectAttribute(object, listName);
+  const list =
+    typeof objectAttribute == 'function' ? objectAttribute() : objectAttribute;
+  node.list = list;
+
+  if (!list) {
+    return;
+  } else if (!(list instanceof Array)) {
+    console.warn(
+      `${object.constructor.name}.${listName} is not an array. Please supply an array.`
+    );
+    return;
+  }
+
+  let { updates, deletions, additions } = updateForLoopElements(
+    node.elements,
+    list
+  );
+
+  //add the stuff
+  for (let i = additions.length - 1; i >= 0; i--) {
+    if (additions[i].content.insertAfterNode) {
+      additions[i].content.insertAfterNode =
+        additions[i].content.insertAfterNode.DOMNode;
+    } else {
+      additions[i].content.insertAfterNode = node.placeholder;
+    }
+    if (!additions[i].content.DOMNode) {
+      additions[i].content = {
+        ...additions[i].content,
+        // merge with for variables
+        ...duplicateNode(node.content),
+      };
+      updates.push(additions[i]);
+    }
+    renderQueue.add(additions[i].content);
+  }
+
+  for (let i = 0; i < deletions.length; i++) {
+    deletions[i].content.toBeDeleted = true;
+    renderQueue.add(deletions[i].content);
+  }
+
+  for (let i = 0; i < updates.length; i++) {
+    updateNode({
+      node: updates[i].content,
+      object: { ...object, ...node.content, [iterator]: updates[i].key },
+    });
+  }
+}
+
+function duplicateNode(content, parentNode) {
+  let newContent = {
+    ...content,
+    children: [],
+    DOMNode: content.DOMNode.cloneNode(false),
+    newDOMNode: content.DOMNode.cloneNode(false),
+  };
+  if (parentNode) {
+    parentNode.appendChild(newContent.DOMNode);
+  }
+  if (content.children) {
+    newContent.children = content.children.map((child) =>
+      duplicateNode(child, newContent.DOMNode)
+    );
+  }
+  return newContent;
+}
+
+function updateForLoopElements(elementPointer, list) {
+  let i = 0;
+  let updates = [];
+  let deletions = [];
+  let additions = [];
+  let lastExistingElement = elementPointer;
+  while (i < list.length) {
+    let inBetween = [elementPointer];
+    let searchPointer = elementPointer.next;
+    while (searchPointer && searchPointer.key !== list[i]) {
+      inBetween.push(searchPointer);
+      searchPointer = searchPointer.next;
+    }
+    if (searchPointer) {
+      if (inBetween.length > 1) {
+        deletions.push(...inBetween.slice(1, inBetween.length));
+      }
+      elementPointer.next = searchPointer;
+      elementPointer = searchPointer;
+      lastExistingElement = searchPointer;
+      updates.push(searchPointer);
+    } else {
+      let newElement = {
+        key: list[i],
+        next: elementPointer.next,
+        content: { insertAfterNode: lastExistingElement.content },
+      };
+
+      let alreadyExistingElementIndex = deletions.findIndex(
+        (element) => element.key === list[i]
+      );
+      if (alreadyExistingElementIndex > -1) {
+        newElement.content = {
+          ...deletions.splice(alreadyExistingElementIndex, 1)[0].content,
+          ...newElement.content,
+        };
+      }
+      elementPointer.next = newElement;
+      elementPointer = newElement;
+      additions.push(newElement);
+    }
+    i++;
+  }
+
+  let inBetween = [elementPointer];
+  let searchPointer = elementPointer.next;
+  while (searchPointer && searchPointer.key !== list[i]) {
+    inBetween.push(searchPointer);
+    searchPointer = searchPointer.next;
+  }
+  if (inBetween.length > 1) {
+    deletions.push(...inBetween.slice(1, inBetween.length));
+  }
+  elementPointer.next = null;
+
+  return { updates, deletions, additions };
+}
+//END-------------------------for node update------------------------------
+
+//BEGIN-------------------------if node update-----------------------------
+function updateIfNode(node, object) {
+  let field = node.expression;
+  let directLogic = true;
+  if (field[0] === '!') {
+    directLogic = false;
+    field = field.substring(1);
+  }
+
+  if (!object.hasOwnProperty(field)) {
+    console.warn(
+      `Possible template issue: the field ${field} is not part of the object ${object.constructor.name}.`
+    );
+  }
+
+  let objectAttribute = getObjectAttribute(object, field);
+  let isTrue =
+    typeof objectAttribute == 'function' ? objectAttribute() : objectAttribute;
+  if (isTrue == directLogic) {
+    if (node.oldDOMNode && node.active !== true) {
+      node.active = true;
+      node.content.newDOMNode = node.oldDOMNode;
+      node.content.skipChildren = false;
+      renderQueue.add(node.content);
+    }
+  } else if (node.active !== false) {
+    node.active = false;
+    node.oldDOMNode = node.content.DOMNode;
+    node.content.newDOMNode = node.placeholder;
+    node.content.skipChildren = true;
+    renderQueue.add(node.content);
+  }
+
+  if (node.active || node.active === null) {
+    updateNode({ node: node.content, object });
+  }
+}
+//END-------------------------if node update-------------------------------
+
+//BEGIN-------------------------text node update---------------------------
+function updateTextNode(node, object) {
+  let text = insertFieldsIntoString(node.text, object);
+  if (node.DOMNode.nodeValue !== text) {
+    node.newDOMNode = node.DOMNode.cloneNode(false);
+    node.newDOMNode.nodeValue = text;
+    renderQueue.add(node);
+  }
+}
+
+function insertFieldsIntoString(string = '', object = {}) {
   let sections = string.split('}');
   let fields = {};
 
   for (let section of sections) {
     let field = section.split('{')[1];
+
     if (field) {
-      fields[field] = object.getAttribute(field);
+      fields[field] = getObjectAttribute(object, field);
     }
   }
 
-  if (isSimpleExpression(string)) {
-    return object.getAttribute(Object.keys(fields)[0]);
-  } else {
-    Object.keys(fields).forEach(key => {
-      string = string.replace(
-        new RegExp(`{${key}}`, 'g'),
-        object.getAttribute(key)
-      );
-    });
-  }
+  Object.keys(fields).forEach((key) => {
+    string = string.replace(
+      new RegExp(`{${key.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}}`, 'g'),
+      getObjectAttribute(object, key)
+    );
+  });
 
   return string;
 }
+//END-------------------------text node update-----------------------------
 
-function getMethodByName(name = '', object) {
-  name = name.substring(1, name.length - 1);
-  return object[name];
-}
-
-function insertFieldsIntoTextNode(node, object) {
-  let text = insertFieldsIntoString(node.text, object);
-  if (node.node.nodeValue !== text) {
-    node.node.nodeValue = text;
-  }
-}
-
-function insertFieldsIntoNodeAttribute(newNode, node, attribute, object) {
-  if (attribute.startsWith('on')) {
-    if (!node.node[attribute]) {
-      newNode.node.removeAttribute(attribute);
-      newNode.node[attribute] = getMethodByName(
-        node.attributes[attribute],
-        object
-      );
-    }
-  } else {
-    newNode.attributes[attribute] = insertFieldsIntoString(
-      node.attributes[attribute],
-      object
-    );
-    if (
-      newNode.node.getAttribute(attribute) !== newNode.attributes[attribute]
-    ) {
-      if (attribute === '*if') {
-        if (newNode.attributes[attribute]) {
-          newNode.placeholder.replaceWith(newNode.node);
-        } else {
-          newNode.node.replaceWith(newNode.placeholder);
-        }
-      } else if (attribute === 'checked') {
-        newNode.node[attribute] = newNode.attributes[attribute];
-      } else {
-        newNode.node.setAttribute(attribute, newNode.attributes[attribute]);
-      }
-    }
-  }
-}
-
-function insertFieldsIntoNode(node, object) {
-  let newNode = {
-    ...node,
-    attributes: {},
-    visible: true
-  };
-
-  if (node.text) {
-    insertFieldsIntoTextNode(newNode, object);
-    return;
-  }
-
-  if (node.instance) {
-    newNode.instance.inputs = newNode.attributes;
-  }
-
-  Object.keys(node.attributes).forEach(attribute =>
-    insertFieldsIntoNodeAttribute(newNode, node, attribute, object)
-  );
-
-  newNode.children.forEach(child => addNodeToRenderQueue(child, object));
-}
-
-function addNodeToRenderQueue(node, object) {
-  if (!node) {
-    return;
-  }
-
-  let index = renderQueue.findIndex(element => element.node === node);
-  if (index > -1) {
-    renderQueue[index] = { node, object };
-  } else {
-    renderQueue.push({ node, object });
-  }
-}
+//BEGIN-------------------------component logic----------------------------
+let componentsList = {};
 
 function toUpperKebabCase(camelCase = '') {
   let nameArray = camelCase.split('');
@@ -149,61 +480,39 @@ function toUpperKebabCase(camelCase = '') {
 }
 
 function loadComponents(...components) {
-  componentsList = {};
-  components.forEach(component => {
+  components.forEach((component) => {
     componentsList[toUpperKebabCase(component.name)] = component;
   });
 }
 
-function workLoop(deadline) {
-  let thereIsStillTime = true;
-  if (renderQueue.length) {
-    while (renderQueue.length && thereIsStillTime) {
-      let elementToRender = renderQueue.shift();
-      insertFieldsIntoNode(elementToRender.node, elementToRender.object);
-
-      thereIsStillTime = deadline.timeRemaining() > 0;
-    }
-  } else if (firstRender) {
-    firstRender();
-    firstRender = null;
-  }
-
-  requestIdleCallback(workLoop);
+function createProxy(object) {
+  return new Proxy(object, {
+    set(target, name, value) {
+      target[name] = value;
+      processQueue.add({
+        node: object.template,
+        object,
+      });
+      return true;
+    },
+  });
 }
 
-requestIdleCallback(workLoop);
-
+function bindClassMethodsToProxy(object, proxy) {
+  let methods = Object.getOwnPropertyNames(Object.getPrototypeOf(object));
+  methods.forEach((method) => {
+    if (typeof object[method] === 'function' && method !== 'constructor') {
+      object[method] = object[method].bind(proxy);
+    }
+  });
+}
 class Component {
+  inputs = {};
   constructor(template) {
     this.template = html2json(template, componentsList);
-
-    this.proxy = new Proxy(this, {
-      set(target, name, value) {
-        target[name] = value;
-        addNodeToRenderQueue(target.template, target);
-        return true;
-      }
-    });
-
-    let methods = Object.getOwnPropertyNames(Object.getPrototypeOf(this));
-    for (let method of methods) {
-      if (typeof this[method] === 'function' && method !== 'constructor') {
-        this[method] = this[method].bind(this.proxy);
-      }
-    }
-
+    this.proxy = createProxy(this);
+    bindClassMethodsToProxy(this, this.proxy);
     return this.proxy;
   }
-
-  getAttribute(propertyPath = '') {
-    let value = this;
-    let properties = propertyPath.split('.');
-    for (let property of properties) {
-      value = value[property];
-    }
-    return value;
-  }
 }
-
-export { Component, renderIntoBody, loadComponents };
+//END-------------------------component logic------------------------------
