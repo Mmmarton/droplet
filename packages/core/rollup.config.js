c4793980b5e1bf3d740f93a13ffb1920ae4cb465
Marton Cserei <marton.cserei@iquestgroup.com>
2020-04-20 18:02:12 +0300

    feature: add uglify to build process

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,14 +1,16 @@
 import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/droplet.js',
   output: {
     file: 'build/droplet.js',
-    format: 'cjs'
+    format: 'cjs',
   },
   plugins: [
     babel({
-      exclude: 'node_modules/**'
-    })
-  ]
+      exclude: 'node_modules/**',
+    }),
+    terser(),
+  ],
 };
